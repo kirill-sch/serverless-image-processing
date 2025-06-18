f0930b08b4ffb0eb2d218f6ed923df9dd51bf8db
kirill-sch <schiroky.kirill@gmail.com>
2025-06-18 23:02:44 +0200

    If you use jsonencode for iam roles or anything else, you can only use template strings with terraform values with locals.

---
+++ Terraform/locals.tf
@@ -0,0 +1,4 @@
+locals {
+  dynamodb_table_arn = "arn:*:dynamodb:${var.region}:${data.aws_caller_identity.current.account_id}:table/${aws_dynamodb_table.image_metadata_table.name}"
+  s3_uploads_arn = "arn:*:s3:::${aws_s3_bucket.image_bucket.bucket}/uploads/*"
+}
